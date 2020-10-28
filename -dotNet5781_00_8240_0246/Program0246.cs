79e17013d262c84569b0ab958f216cc911cfc5c1
hilaa <hilaa@DESKTOP-IH7ML77>
2020-10-28 23:40:39 +0200

    initial code+wellcome0246

---
+++ -dotNet5781_00_8240_0246/Program0246.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet2020_00_0246
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0246();
+            WellcomeYYYY();
+            Console.ReadKey();
+
+
+        }
+
+        private static void WellcomeYYYY()
+        {
+            Console.WriteLine("Im olso here ");
+        }
+
+
+        private static void Welcome0246()
+        {
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
