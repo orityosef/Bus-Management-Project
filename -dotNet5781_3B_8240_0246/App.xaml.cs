9fee9117abdb4d2403c8903e961a090da4dace6e
orityosef <oyosef@g.jct.ac.il>
2020-12-16 22:47:27 +0200

    Ex#03B Final commit

---
+++ -dotNet5781_3B_8240_0246/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _dotNet5781_3B_8240_0246
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
