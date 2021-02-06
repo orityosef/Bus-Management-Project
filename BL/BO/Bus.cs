da4a73d98855f52dfcf29c67e274372096e7ea84
hilaa <hilaa@DESKTOP-IH7ML77>
2021-02-06 22:31:19 +0200

    22:31 6/2/21

--- BL/BO/Bus.cs
+++ BL/BO/Bus.cs
@@ -10,6 +10,36 @@ namespace BO
     public class Bus
     {
         public int LicenseNum { get; set; }
+        public string LicenseN
+        {
+            get
+            {
+                string firstpart, middlepart, endpart;
+                string formattedLicense;
+                string license = LicenseNum.ToString();
+                if (license.Length == 7)
+                {
+                    // xx-xxx-xx
+                    firstpart = license.Substring(0, 2);
+                    middlepart = license.Substring(2, 3);
+                    endpart = license.Substring(5, 2);
+                    formattedLicense = String.Format("{0}-{1}-{2}", firstpart, middlepart, endpart);
+                }
+                else
+                {
+                    // xxx-xx-xxx
+                    firstpart = license.Substring(0, 3);
+                    middlepart = license.Substring(3, 2);
+                    endpart = license.Substring(5, 3);
+                    formattedLicense = String.Format("{0}-{1}-{2}", firstpart, middlepart, endpart);
+                }
+                return String.Format("{0,-10}", formattedLicense);
+            }
+            set
+            {
+                LicenseN = value;
+            }
+        } 
         public DateTime Fromdate { get; set; }
         public double TotalTrip { get; set; }
         public double FuelRemain { get; set; }
@@ -17,27 +47,9 @@ namespace BO
         public Status Status { get; set; }
         public override string ToString()
         {
-            string firstpart, middlepart, endpart;
-            string formattedLicense;
-            string license = LicenseNum.ToString();
-            if (license.Length == 7)
-            {
-                // xx-xxx-xx
-                firstpart = license.Substring(0, 2);
-                middlepart = license.Substring(2, 3);
-                endpart = license.Substring(5, 2);
-                formattedLicense = String.Format("{0}-{1}-{2}", firstpart, middlepart, endpart);
-            }
-            else
-            {
-                // xxx-xx-xxx
-                firstpart = license.Substring(0, 3);
-                middlepart = license.Substring(3, 2);
-                endpart = license.Substring(5, 3);
-                formattedLicense = String.Format("{0}-{1}-{2}", firstpart, middlepart, endpart);
-            }
-            return String.Format("{0,-10}", formattedLicense);
+            return " ";
         }
+
 
     }
 }
