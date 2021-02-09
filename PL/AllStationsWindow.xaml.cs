ba891c63badab5523399aaba89bc290364249b88
hilaa <hilaa@DESKTOP-IH7ML77>
2021-02-09 18:40:22 +0200

    18:40 9/2

--- PL/AllStationsWindow.xaml.cs
+++ PL/AllStationsWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BL.BLAPI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,15 @@ namespace PL
     /// </summary>
     public partial class AllStationsWindow : Window
     {
+
+        readonly IBL bl = BLFactory.GetBL("1");
+
         public AllStationsWindow()
         {
             InitializeComponent();
+            AllStations.ItemsSource = bl.GetAllBusesLine();
+
         }
     }
 }
+
